69798d05355f842f6d8cdc82942fa22351e0b86e
Marlon Estupinan <maestupinan2@poligran.edu.co>
2019-04-07 11:50:54 -0500

    Upload project

---
+++ src/main/java/com/web/start/TechnoAppApplication.java
@@ -0,0 +1,13 @@
+package com.web.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnoAppApplication.class, args);
+	}
+
+}
